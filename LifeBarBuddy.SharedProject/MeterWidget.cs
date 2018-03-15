f4306ee081162be0e890ceeb580f2149931b47b0
Dan Manning <dmanning23@hotmail.com>
2018-03-14 23:19:48 -0500

    added meterwidget

---
+++ LifeBarBuddy.SharedProject/MeterWidget.cs
@@ -0,0 +1,49 @@
+﻿using GameTimer;
+using MenuBuddy;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace LifeBarBuddy
+{
+	public class MeterWidget : Shim
+	{
+		#region Properties
+
+		public IMeter Meter { get; private set; }
+		private IScreen _screen;
+
+		#endregion //Properties
+
+		#region Methods
+
+		public MeterWidget(IScreen screen, IMeter meter)
+			: base(meter.Position.Width, meter.Position.Height)
+		{
+			_screen = screen;
+
+			Meter = meter;
+			SetMeterPosition();
+		}
+
+		public MeterWidget(Shim inst) : base(inst)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Update(IScreen screen, GameClock gameTime)
+		{
+			base.Update(screen, gameTime);
+
+			SetMeterPosition();
+		}
+
+		private void SetMeterPosition()
+		{
+			//update the meter position
+			var position = TransitionObject.Position(_screen.Transition, Rect);
+			Meter.Position = new Rectangle(position.X, position.Y, Meter.Position.Width, Meter.Position.Height);
+		}
+
+		#endregion //Methods
+	}
+}
